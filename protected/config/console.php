d29fd962a78bde55fec4be896e026d335baf4b7e
Marcos Ramos <mjramos.opensev@gmail.com>
2018-10-05 13:09:27 -0300

    atualização

--- protected/config/console.php
+++ protected/config/console.php
@@ -15,15 +15,15 @@ return array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=meubanco',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'Marcos',
+			'password' => '123456',
 			'charset' => 'utf8',
 		),
-		*/
+		
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
